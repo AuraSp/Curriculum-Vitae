4898e395958aa42cab8f9f140029c8a18c5b387c
Aura <80409312+AuraSp@users.noreply.github.com>
2022-03-30 21:06:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function () {
+    $('body').mousemove( (e) => {
+        $('.cursor').css({left: e.clientX, top: e.clientY});
+        $('.cursorInner').css({left: e.clientX, top: e.clientY});
+    });
+
+    $('.music').click(function () {
+        $('i').first().toggleClass('fa-volume-mute');
+        if (audio.paused == false) {
+            audio.pause();
+            audio.currentTime = 0;
+        } else {
+            audio.play();
+        }
+    });
+
+    $('.about').click(function () {
+        console.log('d')
+        window.location.href='./pages/aboutMe.html';
+    })
+});
---
+++ pages/aboutMe.js
@@ -0,0 +1,32 @@
+tsParticles.load({
+  particles: {
+    color: { value: "#f61a16" },
+    move: {
+      direction: "top",
+      enable: true,
+      speed: 0.5
+    },
+    number: {
+      value: 50
+    },
+    opacity: {
+      value: 0.7
+    },
+    shape: {
+      type: "circle"
+    },
+    size: {
+      value: 1.5
+    },
+    zIndex: {
+      value: { min: 0, max: 20 }
+    }
+  }
+});
+
+$(document).ready(function () {
+  $('body').mousemove((e) => {
+    $('.cursor').css({ left: e.clientX, top: e.clientY });
+    $('.cursorInner').css({ left: e.clientX, top: e.clientY });
+  });
+})
