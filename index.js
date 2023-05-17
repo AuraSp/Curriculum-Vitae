c4edd095ea6d6a0158953898ce040608e94a584b
Aura Spejeraite <ArSpej@gmail.com>
2023-05-17 19:24:00 +0300

    fixed some issues

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 
 $(document).ready(function () {
-    $('body').mousemove( (e) => {
-        $('.cursor').css({left: e.clientX, top: e.clientY});
-        $('.cursorInner').css({left: e.clientX, top: e.clientY});
+    $('body').mousemove((e) => {
+        $('.cursor').css({ left: e.clientX, top: e.clientY });
+        $('.cursorInner').css({ left: e.clientX, top: e.clientY });
     });
-
-    // $('.music').click(function () {
-    //     $('i').first().toggleClass('fa-volume-mute');
-    //     if (audio.paused == false) {
-    //         audio.pause();
-    //         audio.currentTime = 0;
-    //     } else {
-    //         audio.play();
-    //     }
-    // });
-
-    // $('.about').click(function () {
-    //     console.log('d')
-    //     window.location.href='./pages/aboutMe.html';
-    // });
 });
